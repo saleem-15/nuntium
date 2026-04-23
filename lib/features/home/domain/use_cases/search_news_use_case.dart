f838a7c2890ead98eda9832917fd36697116bd0c
saleem15 <saleemmahdi10@gmail.com>
2026-04-23 14:23:05 +0300

    feat: update SearchNewsUseCase to reflect saved status

--- lib/features/home/domain/use_cases/search_news_use_case.dart
+++ lib/features/home/domain/use_cases/search_news_use_case.dart
@@ -1,22 +1,32 @@
 import 'package:dartz/dartz.dart';
 import 'package:new_nuntium/core/errors/failures.dart';
 import 'package:new_nuntium/core/models/article.dart';
+import 'package:new_nuntium/features/bookmarks/domain/repository/bookmark_repository.dart';
 import 'package:new_nuntium/features/home/domain/repository/news_repository.dart';
 
 class SearchNewsUseCase {
   final NewsRepository _newsRepository;
+  final BookmarkRepository _bookmarkRepository;
 
-  SearchNewsUseCase(this._newsRepository);
+  SearchNewsUseCase(this._newsRepository, this._bookmarkRepository);
 
   Future<Either<Failure, List<Article>>> call({
     required String query,
     required int page,
     required int pageSize,
   }) async {
-    return await _newsRepository.searchNews(
+    final result = await _newsRepository.searchNews(
       query: query,
       page: page,
       pageSize: pageSize,
+    );
+
+    return result.map(
+      (news) => news.map((article) {
+        return article.copyWith(
+          isSaved: _bookmarkRepository.isArticleSaved(article.id),
+        );
+      }).toList(),
     );
   }
 }
