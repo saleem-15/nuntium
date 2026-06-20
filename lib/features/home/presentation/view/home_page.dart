6a283cf8715c9c5039c96e03791fbeb9189f7eeb
saleem-15 <saleemmahdi10@gmail.com>
2026-06-20 12:04:35 +0300

    refactor(home): migrate routing to navigator and remove controller
    Replaced Get.toNamed with Navigator.pushNamed. Deleted home_controller.dart as state management was already migrated, and removed the unused recent_news_card.dart widget.

--- lib/features/home/presentation/view/home_page.dart
+++ lib/features/home/presentation/view/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:get/get.dart';
+
 import 'package:nuntium/config/routes.dart';
 import 'package:nuntium/core/entities/article.dart';
 import 'package:nuntium/core/resources/app_strings.dart';
@@ -206,6 +206,9 @@ class _HomeViewState extends State<HomeView> {
   }
 
   void _onArticlePressed(Article article) {
-    Get.toNamed(Routes.articleView, arguments: article);
+    Navigator.of(
+      context,
+      rootNavigator: true,
+    ).pushNamed(Routes.articleView, arguments: article);
   }
 }
